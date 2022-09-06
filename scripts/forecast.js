63ce7566cedb13f044a1ec56be69bacd4d1a397f
kapizza20 <103758705+kapizza20@users.noreply.github.com>
2022-09-06 21:24:30 +0200

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -2,7 +2,7 @@ const key='3puo4EnGibgGzRtcNt9d0d1tn0BRhi2u';
 
 //getweaher of that city
 const getWeather= async (id)=>{
-    const base='http://dataservice.accuweather.com/currentconditions/v1/';
+    const base='https://dataservice.accuweather.com/currentconditions/v1/';
     const query=`${id}?apikey=${key}`;
 
     const response=await fetch(base+query);
@@ -13,7 +13,7 @@ const getWeather= async (id)=>{
 
 //getcity information
 const getCity = async (city) =>{
-    const base='http://dataservice.accuweather.com/locations/v1/cities/search';
+    const base='https://dataservice.accuweather.com/locations/v1/cities/search';
     const query=`?apikey=${key}&q=${city}`;
 
     const response = await fetch(base+query);
